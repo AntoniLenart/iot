68f87435e0fe3df5ede435e4256ad002362f5069
altGreG <grzeg.bak.pl@gmail.com>
2025-11-17 12:11:10 +0100

    Add rfid to main loop

--- rpi-zero/main.py
+++ rpi-zero/main.py
@@ -3,6 +3,7 @@ import time
 
 import json
 
+from rfid_module.cardRead import RFIDModule
 
 from fingerprint_module.fingerprintRead import FingerprintModule, DEFAULT_PORT, DEFAULT_BAUD
 import time
@@ -11,7 +12,7 @@ import base64
 from camera_module.qrRead import CameraModule
 from picamera2 import Picamera2
 
-import rfid_module.mqttClient as mqttClient
+import mqtt_module.mqttClient as mqttClient
 import dotenv
 
 import RPi.GPIO as GPIO
@@ -78,6 +79,8 @@ if __name__ == "__main__":
     mqtt.connect()
 
     try:
+        rfid = RFIDModule()
+
         fingerprint = FingerprintModule(port=DEFAULT_PORT, baud=DEFAULT_BAUD, timeout=0)
         picam = Picamera2()
         config = picam.create_video_configuration(
@@ -94,10 +97,49 @@ if __name__ == "__main__":
 
     
         while True:
+            r_uid = rfid.readCard()
             f_uid = fingerprint.get_eigenvalues()
             s_uid = scanner.qr_event.wait(0.1)
 
-            if f_uid:
+            if r_uid:
+                json_request = createJSONRequest("rfid", r_uid)
+                logger.debug(f"Json request: {json_request}")
+                r_uid = None
+                scanner.stop_scan()
+                fingerprint.stop_scan()
+                
+                if mqtt.sendRequest(json_request):
+                    timeoutStart = time.time_ns()
+                    while time.time_ns() - timeoutStart <= DEFAULT_RESPONSE_TIMEOUT_NS:
+                        msg, timestamp = mqtt.getDecision()
+
+                        logger.debug(f"Current message: {msg}")
+                        logger.debug(f"Current timestamp: {timestamp}")
+
+                        print("...")
+
+                        if not msg or not timestamp:
+                            time.sleep(0.2)
+                            continue 
+
+
+                        if timestamp >= timeoutStart and decodeJSONDecision(msg):
+                            # print("allowed")
+                            # GPIO.output(RELAY_PIN, GPIO.HIGH)
+                            time.sleep(5)
+                            # GPIO.output(RELAY_PIN, GPIO.LOW)
+                            break
+                        elif timestamp >= timeoutStart and not decodeJSONDecision(msg):
+                            print("denied")
+                            break
+                        
+                        time.sleep(0.5)
+                else:
+                    print("MQTT ERROR")
+
+                fingerprint.new_scan()
+                scanner.start_background_scan()
+            elif f_uid:
                 json_request = createJSONRequest("fingerprint", f_uid)
                 logger.debug(f"Json request: {json_request}")
                 f_uid = None
@@ -121,7 +163,7 @@ if __name__ == "__main__":
                         if timestamp >= timeoutStart and decodeJSONDecision(msg):
                             print("allowed")
                             #GPIO.output(RELAY_PIN, GPIO.HIGH)
-                            #time.sleep(5)
+                            time.sleep(5)
                             #GPIO.output(RELAY_PIN, GPIO.LOW)
                             break
                         elif timestamp >= timeoutStart and not decodeJSONDecision(msg):
@@ -135,7 +177,6 @@ if __name__ == "__main__":
                 # Start scanning for new fingerprint eigenvalues
                 fingerprint.new_scan()
                 scanner.start_background_scan()
-                time.sleep(5)
             elif s_uid:
                 json_request = createJSONRequest("qr", scanner.qr_result)
                 logger.debug(f"Json request: {json_request}")
@@ -160,7 +201,7 @@ if __name__ == "__main__":
                         if timestamp >= timeoutStart and decodeJSONDecision(msg):
                             print("allowed")
                             #GPIO.output(RELAY_PIN, GPIO.HIGH)
-                            #time.sleep(5)
+                            time.sleep(5)
                             #GPIO.output(RELAY_PIN, GPIO.LOW)
                             break
                         elif timestamp >= timeoutStart and not decodeJSONDecision(msg):
@@ -171,15 +212,11 @@ if __name__ == "__main__":
                 else:
                     print("MQTT ERROR")
 
-
-
-                # Start scanning for new qr code (reset events and qr_result value in class)
                 scanner.start_background_scan()
                 fingerprint.new_scan()
-                time.sleep(5)
-
 
             time.sleep(1)
+
     except Exception:
         fingerprint.stop_scan()
         scanner.stop_scan()
